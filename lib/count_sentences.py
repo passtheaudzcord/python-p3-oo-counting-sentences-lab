b6a23f915fdcd7fe485e02a05d462c9d60633be9
Audrey <chipmanaudrey@gmail.com>
2024-08-19 09:49:49 -0700

    add property decorators

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -5,17 +5,17 @@ class MyString:
     def __init__(self, value = ''):
         self._value = value
         
-    def get_value(self):
+    @property
+    def value(self):
         return self._value
 
-    def set_value(self, value):
+    @value.setter
+    def value(self, value):
         if (type(value) == str):
             print(f"Setting value to {value}")
             self._value = value
         else:
             print("The value must be a string.")
-
-    value = property(get_value, set_value)
 
     def is_sentence(self):
         return self._value.endswith(".")
